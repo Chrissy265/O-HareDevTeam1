d0d938e72cb0442f5701d30f31888919e1b938ca
Christina Beckford <cbeckford70@gmail.com>
2020-02-22 16:18:36 -0500

    Add files via upload
    Assets and fooditems.js

--- fooditems.js
+++ fooditems.js
@@ -1,3 +1,38 @@
+
+
+var config = {
+
+    type: Phaser.AUTO,
+    parent: 'kitchen-scene',
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload(){
+    this.load.multiatlas('kitchenscene',)
+}
+
+window.onload = function(){
+
+    scene: [bootGame, playGame]
+}
+
+class bootGame extends Phaser.Scene{
+    constructor() {
+        super("BootGame");
+    }
+
+    preload(){
+
+    }
+}
+function create(){
 //Create Each Food item with timer functionality 
 
 //Hamburger 
@@ -7,12 +42,12 @@
     isCooked : true, 
     color : "pink",
     onBun : true, 
-    onSkillet : true,
+    onSkillet : true,  
    
     isPattieCooked(){
     if(onSkillet == true)
     {
-        cookingtime();
+        cookingtime(); //create the function under scenes
       
         color = 'brown' ; //represents the pattie is cooked
         isCooked = true;
@@ -56,24 +91,79 @@ var donut ={
 
 //Lvl 2 Food items 
 
-
+var chickenSandwich = {
+    onPlate :true
+};
 //Chicken Sandwich 
 
 //Salad 
 
+var salad = {
+    onBowl: true
+}; 
+
 //Coffee 
+
+var coffee = {
+    isFilled: true
+};
 
 
 //Lvl 3 Food Items
 
-//Grilled Veggies 
+
 
 //BBQ Ribs 
 
+var bbqRibs = {
+    isCooked : true
+};
 
 
+}
 
 
+//Kitchen Tools 
+
+var Skillet = {
+
+    isCooked : false, 
+     onBun :false,
+
+  
+     }, 
+
+
+var Plate = {
+
+    isFooditemOnPlate :false,
+}
+
+var Cup = {
+     isFull : true,
+     isCupAtDispenser: false, 
+}
+
+var Bowl = {
+    isFooditemOnBowl : false,
+}
+
+
+/*var Grill = {
+
+    isFooditemOnGrill: false,
+    cookingTime(); 
+} */
+removeFooditem()
+{
+    //create a function that removes a food item from the kitchen tool
+    //ex if the food item is on a skillet then remove it from there
+}
+
+placeFooditem()
+{
+    //place food item on the kitchen tool 
+}
 
 
 
