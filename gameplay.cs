db57a67607f0ece902f2b8ee55a25412b8c90db0
Abstract410 <56493177+Abstract410@users.noreply.github.com>
2020-02-23 12:40:43 -0600

    Add files via upload

---
+++ gameplay.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class gameplay : MonoBehaviour {
+
+	public static string cuttingboardS1="empty";
+	public static string cuttingboardS2="empty";
+	public static string cuttingboardS3="empty";
+
+	public static string grillS1="empty";
+	public static string grillS2="empty";
+	public static string grillS3="empty";
+}
+
+//use for initialization
+void Start () {
+
+}
+
+//update is celled once per frame
+void Update () {
+
+}
---
+++ gameplay.js
@@ -0,0 +1,9 @@
+//User Input
+var name = window.prompt("Input User Name");
+
+//character function
+$(function() {
+
+    var character = '<div id="character"></div>';
+    $("#game").append(character);
+});
